b976262a77913cf979b70ae631b489c13ca63f6e
Peter Dakin <peterdakin@Peters-MacBook-Pro-2.local>
2026-06-03 13:03:25 +0100

    Update marketing CTA links

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,5 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000";
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? "https://app.origins-id.com";
 export const DEMO_EMAIL = "hello@originsid.com";
